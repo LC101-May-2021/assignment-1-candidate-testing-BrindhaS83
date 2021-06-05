d9b983bd6d8992fa7869103c26456cda49fa5ba9
Brindha Srinivasan <>
2021-06-05 23:29:02 +0000

    changed return statement

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,18 +6,18 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer 
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = 'Who was the first American woman in space? ';
+let correctAnswer ='Sally Ride';
+let candidateAnswer="" ;
 let questions;
 let correctAnswers;
-let candidateAnswers;
+let candidateAnswers='';
 let numberOfCorrAns =0;
 let percScored =0.0;
 let grade;
 //question = "Who was the first American woman in space? ";
 //correctAnswer = "Sally Ride";
-questions = ['1) Who was the first American woman in space?', '2)True or false: 5 kilometer == 5000 meters?','3) (5 + 3)/2 * 10 = ?','4) Given the array[8,Orbit,Trajectory,45],what entry is at index 2?', '5) What is the minimum crew size for the ISS?'];
+questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ',' Given the array[8,Orbit,Trajectory,45],what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
 correctAnswers = ['Sally Ride','true','40','Trajectory','3'];
 //candidateAnswers="";
 candidateAnswers = [""];         
@@ -32,7 +32,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 //candidateAnswer = input.question(question);
   for (i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = input.question(questions[i]);
+    candidateAnswers[i] = input.question(`${i+1})` +  questions[i]);
     let yourAns = `Your Answer:  ${candidateAnswers[i]}`;
     let corrAns = `Correct Answer: ${correctAnswers[i]}`;
   //console.log("Your Answer: "+candidateAnswers[i]);
@@ -60,7 +60,7 @@ function gradeQuiz(candidateAnswers) {
       }
         console.log(">>> Overall Grade: " + percScored +"% (" + numberOfCorrAns + " of " +  questions.length + " responses correct)" + "<<<");
         console.log(">>> Status: " + grade + " <<<");
-        return grade;
+        return percScored;
 }
 
   
